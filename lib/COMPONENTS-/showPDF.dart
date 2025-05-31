6b4f36be551a6237d62a62edb98d91a7dc05cbda
HARI <hariprasath.s@sporadasecure.com>
2025-05-31 10:33:25 +0530

    sfd

--- lib/COMPONENTS-/showPDF.dart
+++ lib/COMPONENTS-/showPDF.dart
@@ -6,7 +6,12 @@ import 'package:ssipl_billing/COMPONENTS-/downloadPDF.dart';
 import 'package:ssipl_billing/COMPONENTS-/sharePDF.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-Widget buildPdfViewer(File file, PdfViewerController controller) {
+Widget buildPdfViewer(
+  File file,
+  PdfViewerController controller,
+  void Function(int)? onPageChanged,
+  void Function(int)? onDocumentLoaded,
+) {
   try {
     return IgnorePointer(
       ignoring: true,
@@ -16,6 +21,8 @@ Widget buildPdfViewer(File file, PdfViewerController controller) {
         enableTextSelection: false,
         canShowScrollHead: false,
         canShowScrollStatus: false,
+        onPageChanged: (details) => onPageChanged?.call(details.newPageNumber),
+        onDocumentLoaded: (details) => onDocumentLoaded?.call(details.document.pages.count),
       ),
     );
   } catch (e) {
@@ -28,89 +35,152 @@ void showPDF(BuildContext context, String filename, File? pdfFile) async {
 
   if (pdfFile != null) {
     final PdfViewerController _pdfController = PdfViewerController();
+    int currentPage = 1;
+    int totalPages = 1;
 
     await showDialog(
       context: context,
-      builder: (context) => Dialog(
-        insetPadding: const EdgeInsets.all(20),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.35,
-          height: MediaQuery.of(context).size.height * 0.95,
-          child: Stack(
-            children: [
-              buildPdfViewer(pdfFile, _pdfController),
-
-              // Navigation Arrows
-              Align(
-                alignment: Alignment.centerLeft,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_left,
-                    size: 30,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    _pdfController.previousPage();
-                  },
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_right,
-                    size: 30,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    _pdfController.nextPage();
-                  },
-                ),
-              ),
-
-              // Bottom download & share buttons
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Dialog(
+              insetPadding: const EdgeInsets.all(20),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.35,
+                height: MediaQuery.of(context).size.height * 0.95,
+                child: Stack(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                    buildPdfViewer(
+                      pdfFile,
+                      _pdfController,
+                      (newPage) {
+                        setState(() {
+                          currentPage = newPage;
+                        });
+                      },
+                      (pages) {
+                        setState(() {
+                          totalPages = pages;
+                        });
+                      },
+                    ),
+
+                    // Navigation Arrows
+                    // Align(
+                    //   alignment: Alignment.centerLeft,
+                    //   child: IconButton(
+                    //     icon: Icon(Icons.arrow_left, size: 30, color: Colors.black),
+                    //     onPressed: () {
+                    //       _pdfController.previousPage();
+                    //     },
+                    //   ),
+                    // ),
+                    // Align(
+                    //   alignment: Alignment.centerRight,
+                    //   child: IconButton(
+                    //     icon: Icon(Icons.arrow_right, size: 30, color: Colors.black),
+                    //     onPressed: () {
+                    //       _pdfController.nextPage();
+                    //     },
+                    //   ),
+                    // ),
+
+                    // Bottom Buttons
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          IconButton(
-                            onPressed: () {
-                              downloadPdf(context, path.basename(filename).replaceAll(RegExp(r'[\/\\:*?"<>|.]'), '').replaceAll(" ", ""), pdfFile);
-                            },
-                            icon: const Icon(Icons.download, color: Colors.green),
+                          // Download & Share
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 5, bottom: 20),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        downloadPdf(
+                                          context,
+                                          path.basename(filename).replaceAll(RegExp(r'[\/\\:*?"<>|.]'), '').replaceAll(" ", ""),
+                                          pdfFile,
+                                        );
+                                      },
+                                      icon: const Icon(Icons.download, color: Colors.green),
+                                    ),
+                                    Text('download', style: TextStyle(fontSize: 12)),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 20, bottom: 20),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        shareAnyPDF(
+                                          context,
+                                          path.basename(filename).replaceAll(RegExp(r'[\/\\:*?"<>|.]'), '').replaceAll(" ", ""),
+                                          pdfFile,
+                                        );
+                                      },
+                                      icon: const Icon(Icons.share, color: Colors.blue),
+                                    ),
+                                    Text('share', style: TextStyle(fontSize: 12)),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                          Text('download'),
+
+                          // Page Indicator
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 2, bottom: 10),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        _pdfController.previousPage();
+                                      },
+                                      icon: const Icon(Icons.arrow_left_sharp, color: Colors.black),
+                                    ),
+                                    Text(
+                                      'Page ($currentPage / $totalPages)',
+                                      style: TextStyle(fontSize: 12),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        _pdfController.nextPage();
+                                      },
+                                      icon: const Icon(Icons.arrow_right_sharp, color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )
                         ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              shareAnyPDF(context, path.basename(filename).replaceAll(RegExp(r'[\/\\:*?"<>|.]'), '').replaceAll(" ", ""), pdfFile);
-                            },
-                            icon: const Icon(Icons.share, color: Colors.blue),
-                          ),
-                          Text('share'),
-                        ],
-                      ),
-                    )
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            );
+          },
+        );
+      },
     );
+
+    // Wait for total pages to be available
+    Future.delayed(Duration(milliseconds: 700), () async {
+      totalPages = _pdfController.pageCount;
+    });
   }
 }
